51b4482f5916d2d88ba4f1615017ab3d1b463ed9
Dmitriy1892 <kodim1892@gmail.com>
2020-09-24 10:49:30 +0300

    PizzaDetailActivity added

--- app/src/main/java/com/hfad/bitsandpizzas/CaptionedImageAdapter.java
+++ app/src/main/java/com/hfad/bitsandpizzas/CaptionedImageAdapter.java
@@ -1,6 +1,9 @@
 package com.hfad.bitsandpizzas;
 
 import android.graphics.drawable.Drawable;
+import android.location.GpsStatus;
+import android.net.sip.SipAudioCall;
+import android.net.sip.SipSession;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,12 +12,18 @@ import android.widget.TextView;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
 
 public class CaptionedImageAdapter extends
         RecyclerView.Adapter<CaptionedImageAdapter.ViewHolder> {
 
     private String[] captions;
     private int[] imageIds;
+    private Listener listener;
+
+    interface Listener {
+        void onClick(int position);
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
 
@@ -36,6 +45,10 @@ public class CaptionedImageAdapter extends
         return captions.length;
     }
 
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
     @Override
     public CaptionedImageAdapter.ViewHolder onCreateViewHolder(
             ViewGroup parent, int viewType) {
@@ -45,7 +58,7 @@ public class CaptionedImageAdapter extends
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
         CardView cardView = holder.cardView;
         ImageView imageView = (ImageView) cardView.findViewById(R.id.info_image);
         Drawable drawable =
@@ -54,5 +67,13 @@ public class CaptionedImageAdapter extends
         imageView.setContentDescription(captions[position]);
         TextView textView = (TextView)cardView.findViewById(R.id.info_text);
         textView.setText(captions[position]);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (listener !=null) {
+                    listener.onClick(position);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/hfad/bitsandpizzas/PizzaDetailActivity.java
@@ -0,0 +1,37 @@
+package com.hfad.bitsandpizzas;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+public class PizzaDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_PIZZA_ID = "pizzaId";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        int pizzaId = (Integer)getIntent().getExtras().get(EXTRA_PIZZA_ID);
+        String pizzaName = Pizza.pizzas[pizzaId].getName();
+        TextView textView = (TextView) findViewById(R.id.pizza_text);
+        textView.setText(pizzaName);
+        int pizzaImage = Pizza.pizzas[pizzaId].getImageResourseId();
+        ImageView imageView = (ImageView) findViewById(R.id.pizza_image);
+        imageView.setImageDrawable(ContextCompat.getDrawable(this, pizzaImage));
+        imageView.setContentDescription(pizzaName);
+    }
+}
